34d90d4c52a62d29983808f8af95a9d51e16943b
tyczof <tyczka.zosia@gmail.com>
2025-04-28 13:24:14 +0200

    Added ref to mail in buttons

--- src/components/Hero/HeroButton.tsx
+++ src/components/Hero/HeroButton.tsx
@@ -1,7 +1,7 @@
 const HeroButton = () => {
   return (
     <button className="z-0 px-6 py-3 border border-struzka rounded-full text-struzka text-sm hover:bg-bedge-20 hover:text-bedge-60 hover:border-bedge-60 hover:cursor-pointer transition">
-      Zarezerwuj swój pobyt
+      <a href="mailto:someone@example.com">Zarezerwuj swój pobyt</a>
     </button>
   );
 };
